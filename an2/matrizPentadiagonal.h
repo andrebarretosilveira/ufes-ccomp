b1674b59f96cbb77fee64f91aac4d01a90e70038
dellief <andre.dellief@gmail.com>
2016-04-13 23:13:54 -0300

    Iniciando T1 de AN2

---
+++ an2/matrizPentadiagonal.h
@@ -0,0 +1,35 @@
+/**
+ * matrizPentadiagonal.h
+ *
+ * Definição da estrutura da Matriz Pentadiagonal
+ * e suas funções básicas.
+ *
+ */
+
+#ifndef MATRIZPENTADIAGONAL_H
+#define MATRIZPENTADIAGONAL_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * Estrutura da matriz composta por 5
+ * vetores, representando as diagonais
+ */
+typedef struct matrizPentadiagonal
+{
+	double *e;
+	double *c;
+	double *a;
+	double *b;
+	double *d;
+    size_t n;
+} MatrizPentadiagonal;
+
+/** Funções **/
+MatrizPentadiagonal* newMatPen(int);
+void preencheMatPen(MatrizPentadiagonal*);
+void printMatPen(MatrizPentadiagonal*);
+void freeMatPen(MatrizPentadiagonal*);
+
+#endif
---
+++ an2/trab1.c
@@ -0,0 +1,25 @@
+/**
+ * trab1.c
+ *
+ * Testador do Trabalho 1 de Algoritmos
+ * Numéricos: Método das Diferencas Finitas
+ * Aplicado a Problemas Bidimensionais
+ *
+ * Autor: André Barreto Silveira
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrizPentadiagonal.h"
+
+int main()
+{
+    MatrizPentadiagonal* matriz;
+    matriz = newMatPen(15);
+    preencheMatPen(matriz);
+    printMatPen(matriz);
+    freeMatPen(matriz);
+
+    return 0;
+}
