650a83d6b2211aaa2054dcf2d9e2b7341d5b0c10
dellief <andre.dellief@gmail.com>
2016-09-26 18:27:34 -0300

    Trabalho bem estruturado. Classes Swarm, Particle e PSO implementadas. Falta encontrar o(s) erro(s).

--- ia/trab1/src/algorithm/Swarm.java
+++ ia/trab1/src/algorithm/Swarm.java
@@ -3,25 +3,68 @@ package algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+import jmetal.core.Problem;
+import jmetal.core.Solution;
+import jmetal.util.JMException;
+
 public class Swarm {
 	
 	private PSOAlgorithm pso;
-	private List<Double> bestPosition;
+	private Solution bestSolution;
 	private List<Particle> particles;
 	
-	public Swarm(PSOAlgorithm pso, Integer dimensions, Integer numberOfParticles) {
+	public Swarm(PSOAlgorithm pso, Problem problem, Integer numberOfParticles)
+			throws ClassNotFoundException, JMException {
 		this.pso = pso;
-		bestPosition = new ArrayList<Double>(dimensions);
-		particles = new ArrayList<Particle>(numberOfParticles);
+		this.bestSolution = new Solution(problem);
+		this.particles = new ArrayList<Particle>(numberOfParticles);
 		
 		// Criando as Partículas do Enchame
 		for(int i=0; i < numberOfParticles; i++) {
-			particles.add(i, new Particle(this, dimensions));
+			this.particles.add(i, new Particle(this, problem));
+		}
+		
+		// Inicializando a melhor solução aleatoriamente
+		this.bestSolution.setObjective(0, 
+				particles.get(0).getBestSolution().getObjective(0));
+		this.bestSolution.setDecisionVariables(particles.get(0).
+				getBestSolution().getDecisionVariables().clone());
+	}
+	
+	public void evaluateParticlesFitness() throws JMException {
+		for (Particle particle : this.particles) {
+			particle.evaluateFitness();
+		}
+		
+	}
+	
+	public void updateBests() {
+		for (Particle particle : particles) {
+			Solution individualBest = particle.updateIndividualBest();
+			if(individualBest.getObjective(0) < this.bestSolution.getObjective(0)) {
+				this.bestSolution.setObjective(0, individualBest.getObjective(0));
+				this.bestSolution.setDecisionVariables(
+						individualBest.getDecisionVariables().clone());
+			}
+		}
+		
+		System.out.println("\nBest Global: " + this.bestSolution.getObjective(0));
+	}
+	
+	public void updateParticlesVelocityPosition() throws JMException {
+		for (Particle particle : particles) {
+			particle.updateVelocity();
+			particle.updatePosition();
 		}
 	}
 
 	public PSOAlgorithm getPso() {
 		return pso;
 	}
+	
+	public Solution getBestSolution() {
+		return bestSolution;
+	}
+
 
 }
