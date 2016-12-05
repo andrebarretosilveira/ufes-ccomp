caf3751455f3ffad213293f748239c8b3aab632f
dellief <andre.dellief@gmail.com>
2016-12-05 15:08:53 -0200

    Começando trab3 de IA

---
+++ ia/trab3/src/main/java/com/sample/Animal.java
@@ -0,0 +1,51 @@
+package com.sample;
+
+public class Animal {
+	
+	private int habitat;
+	private int revestimento;
+	private int locomocao;
+	private int reproducao;
+	private int alimentacao;
+	private int respiracao;
+	
+	public int getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(int habitat) {
+		this.habitat = habitat;
+	}
+	public int getRevestimento() {
+		return revestimento;
+	}
+	public void setRevestimento(int revestimento) {
+		this.revestimento = revestimento;
+	}
+	public int getLocomocao() {
+		return locomocao;
+	}
+	public void setLocomocao(int locomocao) {
+		this.locomocao = locomocao;
+	}
+	public int getReproducao() {
+		return reproducao;
+	}
+	public void setReproducao(int reproducao) {
+		this.reproducao = reproducao;
+	}
+	public int getAlimentacao() {
+		return alimentacao;
+	}
+	public void setAlimentacao(int alimentacao) {
+		this.alimentacao = alimentacao;
+	}
+	public int getRespiracao() {
+		return respiracao;
+	}
+	public void setRespiracao(int respiracao) {
+		this.respiracao = respiracao;
+	}
+	
+	
+
+}
